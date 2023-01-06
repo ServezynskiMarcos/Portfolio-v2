22ab26fce4540648536979c9e44c71d3450cfd88
Servezynski <servezynskimarcos1@gmail.com>
2023-01-06 19:30:00 -0300

    change cv

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -92,7 +92,7 @@ const Footer = () => {
             <Icon as={AiFillInstagram} color={iconColor} />
           </Stack>
         </a>
-        <a href="https://drive.google.com/uc?id=1NvdF4nEV77bi5kEF3_5V7hFxorNvgZlk&export=download">
+        <a href="https://drive.google.com/uc?id=1T2JoWCFg2dKg8xNodIWMrfAE7220aGoW&export=download">
           <Stack
             alignItems={"center"}
             direction={"row"}
